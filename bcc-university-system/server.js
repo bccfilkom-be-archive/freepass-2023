ba6367e3eab50ee68d5d062746742027055829df
Irfan Hanif <14.irfanh@gmail.com>
2023-01-25 17:06:34 +0700

    formatted files

--- bcc-university-system/server.js
+++ bcc-university-system/server.js
@@ -1,3 +1,3 @@
-const express = require('express')
+const express = require("express");
 
-const db = require('./util/database')
+const db = require("./util/database");
--- bcc-university-system/util/database.js
+++ bcc-university-system/util/database.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
-exports.db = mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).catch(err => console.error(err));
+exports.db = mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.error(err));
