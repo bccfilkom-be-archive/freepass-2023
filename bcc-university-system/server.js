11f59bc40fd86151c3fbacd2781c6891e3735e43
Irfan Hanif <14.irfanh@gmail.com>
2023-01-26 15:57:22 +0700

    edited server file

--- bcc-university-system/server.js
+++ bcc-university-system/server.js
@@ -1,17 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const db = require("./util/database");
-const routes = require('./routes/index')
+const routes = require("./routes/index.routes");
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.use(cors)
-app.use(express.urlencoded({extended: true}))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', routes)
+app.use("/api", routes);
 
 app.listen(port, () => {
-    console.log(`Server is listening in port ${port}...`);
-})
+  console.log(`Server is listening in port ${port}...`);
+});
