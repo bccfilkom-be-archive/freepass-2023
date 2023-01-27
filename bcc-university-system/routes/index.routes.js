ac6c1fc89ffd95463f135d4a94eee6bdf4b4fd2c
Irfan Hanif <14.irfanh@gmail.com>
2023-01-27 19:10:24 +0700

    updated index route

--- bcc-university-system/routes/index.routes.js
+++ bcc-university-system/routes/index.routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/users", require('./user.routes'));
-// router.use("/admin");
-// router.use("/course");
+router.use("/admin", require('./admin.routes'));
+router.use("/courses", require('./course.routes'));
 
 module.exports = router;
