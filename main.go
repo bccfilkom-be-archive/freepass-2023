7284a162e83466ba4efd785655803756cdcff0a7
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-31 18:42:04 +0700

    test run Gin

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"freepass-2023/initializers"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
-	fmt.Println("Hello")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
