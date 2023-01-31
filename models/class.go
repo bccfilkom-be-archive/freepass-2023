d68e3555bda1b8416ea60ed285c6171bccb56e9a
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-31 20:52:34 +0700

    undo comment

--- models/class.go
+++ models/class.go
@@ -2,7 +2,6 @@ package models
 
 import "gorm.io/gorm"
 
-// Class is a struct that represents a class entity.
 type Class struct {
 	ClassName string   `json:"class_name" gorm:"not null;unique;primaryKey"`
 	Courses   []Course `json:"courses"`
