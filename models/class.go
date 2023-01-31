c7d63801725b5845c319dad19665b85e6ddfbfd4
MirzaHilmi <exquisitemirza@student.ub.ac.id>
2023-01-31 21:27:49 +0700

    restructure class struct

--- models/class.go
+++ models/class.go
@@ -3,9 +3,10 @@ package models
 import "gorm.io/gorm"
 
 type Class struct {
-	ClassName string   `json:"class_name" gorm:"not null;unique;primaryKey"`
-	Courses   []Course `json:"courses"`
-	Enrolled  int      `json:"enrolled" gorm:"check:enrolled <= capacity"`
-	Students  []User   `json:"students"`
+	ClassID   string `json:"class_id" gorm:"not null;unique;primaryKey"`
+	ClassName string `json:"class_name" gorm:"not null;unique"`
+	Credits   int    `json:"credits" gorm:"check:credits > 0;check:credits < 7"`
+	Enrolled  int    `json:"enrolled" gorm:"check:enrolled <= capacity"`
+	Students  []User `json:"students"`
 	gorm.Model
 }
