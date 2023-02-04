6d0e4d14937f36892874747f6d83ee0e137a0365
Guspascad420 <Paacad@gmail.com>
2023-02-04 20:10:14 +0800

    Changed Participants datatype from hashmap to array of ObjectID's

--- models/class.go
+++ models/class.go
@@ -2,19 +2,20 @@ package models
 
 import (
 	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Class struct {
 	mgm.DefaultModel `bson:",inline"`
-	Title       		string 						`json:"title" bson:"title"`
-	Sks					int32    					`json:"sks" bson:"sks"`
-	Participants		map[string]bool				`json:"participants" bson:"participants"`
+	Title            string               `json:"title" bson:"title"`
+	Sks              int32                `json:"sks" bson:"sks"`
+	Participants     []primitive.ObjectID `json:"participants" bson:"participants"`
 }
 
 func NewClass(title string, sks int32) *Class {
 	return &Class{
-		Title: title,
-		Sks: 3,
-		Participants: map[string]bool{},
+		Title:        title,
+		Sks:          sks,
+		Participants: []primitive.ObjectID{},
 	}
 }
