3aef07a063a4bf5e57ca2cd344e7b4475884c251
Guspascad420 <Paacad@gmail.com>
2023-02-04 20:15:32 +0800

    Changed Classes_Enrolled name to CamelCase (ClassesEnrolled) and its datatype from hashmap to array of primitive.ObjectID's

--- models/user.go
+++ models/user.go
@@ -2,28 +2,29 @@ package models
 
 import (
 	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
 	// DefaultModel adds _id, created_at and updated_at fields to the Model
 	mgm.DefaultModel `bson:",inline"`
-	Username         		string 							`json:"username" bson:"username"`
-	Email            		string 							`json:"email" bson:"email"`
-	Password         		string 							`json:"password" bson:"password"`
-	Groups					string 							`json:"groups" bson:"groups"`
-	First_Name		 		string 							`json:"first_name" bson:"first_name"`
-	Last_Name		 		string 							`json:"last_name" bson:"last_name"`
-	Classes_Enrolled 		map[string]int32				`json:"classes_enrolled" bson:"classes_enrolled"`
-	Rem_sks					int32							`json:"rem_sks" bson:"rem_sks"`
+	Username         string               `json:"username" bson:"username"`
+	Email            string               `json:"email" bson:"email"`
+	Password         string               `json:"password" bson:"password"`
+	Groups           string               `json:"groups" bson:"groups"`
+	FirstName        string               `json:"first_name" bson:"first_name"`
+	LastName         string               `json:"last_name" bson:"last_name"`
+	ClassesEnrolled  []primitive.ObjectID `json:"classes_enrolled" bson:"classes_enrolled"`
+	RemSks           int32                `json:"rem_sks" bson:"rem_sks"`
 }
 
 type UserDetails struct {
-	Username       	 		string 							`json:"username,omitempty"`
-	Email     		 		string 							`json:"email,omitempty"`
-	First_Name		 		string 							`json:"first_name,omitempty"`
-	Last_Name		 		string 							`json:"last_name,omitempty"`
-	Classes_Enrolled 		map[string]int32				`json:"classes_enrolled,omitempty"`
-	Rem_sks					int32							`json:"rem_sks,omitempty"`
+	Username        string               `json:"username,omitempty"`
+	Email           string               `json:"email,omitempty"`
+	FirstName       string               `json:"first_name,omitempty"`
+	LastName        string               `json:"last_name,omitempty"`
+	ClassesEnrolled []primitive.ObjectID `json:"classes_enrolled,omitempty"`
+	RemSks          int32                `json:"rem_sks,omitempty"`
 }
 
 func NewUser(username string, email string, password string) *User {
@@ -31,6 +32,6 @@ func NewUser(username string, email string, password string) *User {
 		Username: username,
 		Email:    email,
 		Password: password,
-		Rem_sks: 24,
+		RemSks:   24,
 	}
 }
